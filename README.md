9d8aeacf263e082fdfd1372b790a9fc4eb93111b
Anton Shramko <antonshramko@yandex.ru>
2024-07-18 17:35:59 +0300

    Initial commit. Component separated from mielo-react.

---
+++ README.md
@@ -0,0 +1,64 @@
+# Mielo React Forms
+
+Component wrappers over forms for `react-hook-form`.
+
+## Quick install
+
+Mielo is constantly in development! Try it out now:
+
+### NPM
+
+```sh
+npm install mielo mielo-react-forms
+```
+
+**or**
+
+### Yarn
+
+```sh
+npm install mielo mielo-react-forms
+```
+
+### Import
+
+After installation, you can import the CSS file into your project using this snippet:
+
+```sh
+@import 'mielo/css/mielo.css'
+```
+
+And usage react components like
+
+``` typescript
+import { FormEntry } from "mielo-react-slider"
+
+<FormEntry ... />
+```
+
+## Browser Support
+
+Mielo is compatible with **recent** versions of:
+
+- Chrome
+- Edge
+- Firefox
+- Opera
+- Safari
+
+Internet Explorer is not supported.
+
+## Documentation
+Browse the [online documentation here.](#todo)
+
+# Development
+For build library from source:
+
+``` sh
+git clone https://github.com/friktor/mielo-react-forms
+cd mielo-react-forms
+
+npm install # or "yarn"
+npm run fmt # format & linting components
+npm build # build module library with .d.ts
+```
---
+++ src/FormSelect.tsx
@@ -0,0 +1,47 @@
+import { Control, Controller, RegisterOptions } from "react-hook-form"
+import { Option, Select } from "mielo-react"
+
+export interface FormSelectProps {
+  rules?: Omit<
+    RegisterOptions<any, string>,
+    "valueAsNumber" | "valueAsDate" | "setValueAs" | "disabled"
+  >
+
+  control: Control<any, any>
+  placeholder: string
+  disabled?: boolean
+  compact?: boolean
+  options: Option[]
+  name: string
+}
+
+export function FormSelect({
+  placeholder,
+  options,
+  disabled,
+  control,
+  rules,
+  name,
+}: FormSelectProps) {
+  return (
+    <div></div>
+    // <Controller
+    //   control={control}
+    //   rules={rules}
+    //   name={name}
+    //   render={({ field: { onChange, value }, fieldState: { error } }) => (
+    //     <Select
+    //       placeholder={placeholder}
+    //       error={error?.message}
+    //       disabled={disabled}
+    //       onChange={onChange}
+    //       options={options}
+    //       value={value}
+    //       name={name}
+    //     />
+    //   )}
+    // />
+  )
+}
+
+FormSelect.displayName = "Mie.Form.Select"
